0694bd31eeaba105a184e5e64c011d6b010cb277
aarown00 <aarown.00@gmail.com>
2025-06-23 00:14:47 +0800

    database starting template

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CustomerDetail, PurchaseOrder
 
-# Register your models here.
+admin.site.register(CustomerDetail)
+admin.site.register(PurchaseOrder)
--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class CustomerDetail(models.Model):
+    customer_name = models.CharField(max_length=100)
+    branch_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.branch_name}"
+
+class PurchaseOrder(models.Model):
+    STATUS_CHOICES = [
+        ('Ongoing', 'Ongoing'),
+        ('Completed', 'Completed'),
+        ('Delayed', 'Delayed'),
+    ]
+
+    REMARKS_CHOICES = [
+        ('FOR BILLING', 'FOR BILLING'),
+        ('FOR SIGNATURE', 'FOR SIGNATURE'),
+    ]
+
+    purchase_order = models.CharField(max_length=100)
+    date_in_email = models.DateField()
+    customer_branch = models.ForeignKey(CustomerDetail, on_delete=models.CASCADE)
+
+    total_days = models.PositiveIntegerField()
+    manpower_count = models.PositiveIntegerField()
+    time_total = models.PositiveIntegerField()
+
+    # Automatically calculate work hours
+    @property
+    def total_work_hour(self):
+        return self.manpower_count * self.time_total
+
+    description = models.TextField()
+    remarks = models.CharField(max_length=20, choices=REMARKS_CHOICES)
+    date_started = models.DateField()
+    target_date = models.DateField()
+    completion_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.purchase_order}"
