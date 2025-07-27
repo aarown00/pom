ff1310c11170cea04cf6b2ebe0e5f463a7bdc90b
aarown00 <aarown.00@gmail.com>
2025-07-28 06:51:02 +0800

    csrf test

--- mabuhaypowers_pom/settings.py
+++ mabuhaypowers_pom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-8^h_ap8lvjlmxrd8gc=0b6(x!)$*ie-jd#0-l1tkbly@@iuady
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://mabuhayops.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 
 # Application definition
